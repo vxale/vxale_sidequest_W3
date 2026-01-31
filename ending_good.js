a1b2a99cfed8d904ec8b81e976ea7284a52e04c6
Frank Le <vxale@uwaterloo.ca>
2026-01-31 17:07:46 -0500

    Create ending_good.js
    good ending added

---
+++ ending_good.js
@@ -0,0 +1,45 @@
+const restartBtn = {
+  x: width / 2,
+  y: 600,
+  w: 260,
+  h: 80,
+  label: "RESTART",
+};
+
+function drawEndingGood() {
+  background(200, 235, 255);
+
+  fill(0);
+  textAlign(CENTER, CENTER);
+
+  textSize(38);
+  text("Good Ending", width / 2, 270);
+
+  textSize(18);
+  text(
+    "You slow down and ask if they’re okay.\n" +
+      "They’re lost, phone dead — trying to find the right bus stop.\n\n" +
+      "You point them the right way, wait until they feel steady,\n" +
+      "and they thank you like it mattered.\n\n" +
+      "On the rest of your walk, you feel the weight of your choice:\n" +
+      "attention is a kind of care.",
+    width / 2,
+    390,
+  );
+
+  drawStoryButton(restartBtn);
+  cursor(isHover(restartBtn) ? HAND : ARROW);
+
+  textSize(18);
+  text("Click or press the SPACEBAR to restart the story.", width / 2, 650);
+}
+
+function endingMousePressed() {
+  if (isHover(restartBtn)) {
+    currentScreen = "start";
+  }
+}
+
+function endingKeyPressed() {
+  if (key === " ") currentScreen = "start";
+}
